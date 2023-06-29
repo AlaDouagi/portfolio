d5ef6a1f8af0fa725194db44c5579e3251809d2e
Alaeddine Douagi <ala.douagi@gmail.com>
2023-06-29 17:30:51 +0200

    Fix compose script

--- scripts/compose.js
+++ scripts/compose.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const inquirer = require('inquirer');
 const dedent = require('dedent');
 
 const root = process.cwd();
@@ -12,7 +11,7 @@ const getAuthors = () => {
 };
 
 const getLayouts = () => {
-  const layoutPath = path.join(root, 'layouts');
+  const layoutPath = path.join(root, 'layouts/MDX');
   const layoutList = fs
     .readdirSync(layoutPath)
     .map((filename) => path.parse(filename).name)
@@ -51,71 +50,75 @@ const genFrontMatter = (answers) => {
   return frontMatter;
 };
 
-inquirer
-  .prompt([
-    {
-      name: 'title',
-      message: 'Enter post title:',
-      type: 'input',
-    },
-    {
-      name: 'extension',
-      message: 'Choose post extension:',
-      type: 'list',
-      choices: ['mdx', 'md'],
-    },
-    {
-      name: 'authors',
-      message: 'Choose authors:',
-      type: 'checkbox',
-      choices: getAuthors,
-    },
-    {
-      name: 'summary',
-      message: 'Enter post summary:',
-      type: 'input',
-    },
-    {
-      name: 'draft',
-      message: 'Set post as draft?',
-      type: 'list',
-      choices: ['yes', 'no'],
-    },
-    {
-      name: 'tags',
-      message: 'Any Tags? Separate them with , or leave empty if no tags.',
-      type: 'input',
-    },
-    {
-      name: 'layout',
-      message: 'Select layout',
-      type: 'list',
-      choices: getLayouts,
-    },
-  ])
-  .then((answers) => {
-    // Remove special characters and replace space with -
-    const fileName = answers.title
-      .toLowerCase()
-      .replace(/[^a-zA-Z0-9 ]/g, '')
-      .replace(/ /g, '-')
-      .replace(/-+/g, '-');
-    const frontMatter = genFrontMatter(answers);
-    const filePath = `data/blog/${fileName ? fileName : 'untitled'}.${
-      answers.extension ? answers.extension : 'md'
-    }`;
-    fs.writeFile(filePath, frontMatter, { flag: 'wx' }, (err) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log(`Blog post generated successfully at ${filePath}`);
-      }
-    });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log("Prompt couldn't be rendered in the current environment");
-    } else {
-      console.log('Something went wrong, sorry!');
-    }
-  });
+import('inquirer')
+  .then((i) => i.default)
+  .then((inquirer) =>
+    inquirer
+      .prompt([
+        {
+          name: 'title',
+          message: 'Enter post title:',
+          type: 'input',
+        },
+        {
+          name: 'extension',
+          message: 'Choose post extension:',
+          type: 'list',
+          choices: ['mdx', 'md'],
+        },
+        {
+          name: 'authors',
+          message: 'Choose authors:',
+          type: 'checkbox',
+          choices: getAuthors,
+        },
+        {
+          name: 'summary',
+          message: 'Enter post summary:',
+          type: 'input',
+        },
+        {
+          name: 'draft',
+          message: 'Set post as draft?',
+          type: 'list',
+          choices: ['yes', 'no'],
+        },
+        {
+          name: 'tags',
+          message: 'Any Tags? Separate them with , or leave empty if no tags.',
+          type: 'input',
+        },
+        {
+          name: 'layout',
+          message: 'Select layout',
+          type: 'list',
+          choices: getLayouts,
+        },
+      ])
+      .then((answers) => {
+        // Remove special characters and replace space with -
+        const fileName = answers.title
+          .toLowerCase()
+          .replace(/[^a-zA-Z0-9 ]/g, '')
+          .replace(/ /g, '-')
+          .replace(/-+/g, '-');
+        const frontMatter = genFrontMatter(answers);
+        const filePath = `data/blog/${fileName ? fileName : 'untitled'}.${
+          answers.extension ? answers.extension : 'md'
+        }`;
+        fs.writeFile(filePath, frontMatter, { flag: 'wx' }, (err) => {
+          if (err) {
+            throw err;
+          } else {
+            console.log(`Blog post generated successfully at ${filePath}`);
+          }
+        });
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          console.log("Prompt couldn't be rendered in the current environment");
+        } else {
+          console.log('Something went wrong, sorry!');
+        }
+      })
+  );
